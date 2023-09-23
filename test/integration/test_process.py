7f9242609c69722910176371a1cd3b1a14998945
TJ <none@none.com>
2023-09-23 07:31:48 -0400

    Add testing of separate process stages/SAM input.

--- test/integration/test_process.py
+++ test/integration/test_process.py
@@ -49,6 +49,12 @@ class TestProcess(unittest.TestCase):
       os.path.join(os.path.dirname(__file__), 'input', 'fastq', 'Sense_R1_3.fq'),
       os.path.join(os.path.dirname(__file__), 'input', 'fastq', 'Sense_R1_4.fq'),
     ]
+    input_sam = [
+      os.path.join(os.path.dirname(__file__), 'input', 'sam', 'Sense_R1_1.sam'),
+      os.path.join(os.path.dirname(__file__), 'input', 'sam', 'Sense_R1_2.sam'),
+      os.path.join(os.path.dirname(__file__), 'input', 'sam', 'Sense_R1_3.sam'),
+      os.path.join(os.path.dirname(__file__), 'input', 'sam', 'Sense_R1_4.sam'),
+    ]
     ref = os.path.join(os.path.dirname(__file__), 'input', 'ref_seq', '2DSB_Sense_R1.fa')
     output = os.path.join(os.path.dirname(__file__), 'output', 'Sense_R1')
     output_expected = os.path.join(os.path.dirname(__file__), 'output_expected', 'Sense_R1')
@@ -58,15 +64,52 @@ class TestProcess(unittest.TestCase):
     if os.path.exists(output):
       shutil.rmtree(output)
     
-    self.assertEqual(
-      os.system(
-        'DSBplot-process -o {} -i {} {} {} {} --ref {} --dsb 67 --label "Sense (R1)" --reads {} {} {} {}'
-        .format(output, *input, ref, *reads)
-      ),
-      0,
-      'DSBplot-process failed.'
+    command = (
+      'DSBplot-process -o {} -i {} {} {} {} --ref {} --dsb 67 --reads {} {} {} {}'
+      .format(output, *input, ref, *reads)
     )
+    print('Testing command: ' + command)
+    self.assertEqual(os.system(command), 0, 'DSBplot-process failed.')
 
+    # Make sure output is the same as expected
+    for file in TestProcess.get_check_files():
+      print('Testing file: ' + file)
+      self.check_equality(os.path.join(output, file), os.path.join(output_expected, file))
+
+    # Clear output directory (if exists)
+    if os.path.exists(output):
+      shutil.rmtree(output)
+
+    # Now do the steps separately and make sure output is the same
+    command = [
+      'DSBplot-process -o {} -i {} {} {} {} --ref {} --stages 0'.format(output, *input, ref),
+      'DSBplot-process -o {} --ref {} --dsb 67 --reads {} {} {} {} --stages 1_filter'.format(output, ref, *reads),
+      'DSBplot-process -o {} --stages 2_window'.format(output),
+      'DSBplot-process -o {} --stages 3_variation'.format(output),
+      'DSBplot-process -o {} --stages 4_info'.format(output),
+    ]
+    for i in range(len(command)):
+      print('Testing command: ' + command[i])
+      self.assertEqual(os.system(command[i]), 0, 'DSBplot-process failed.')
+    
+    # Make sure output is the same as expected
+    for file in TestProcess.get_check_files():
+        print('Testing file: ' + file)
+        self.check_equality(os.path.join(output, file), os.path.join(output_expected, file))
+
+    # Clear output directory (if exists)
+    if os.path.exists(output):
+      shutil.rmtree(output)
+
+    # Now do the steps with the precomputed SAM files and make sure output is the same
+    command = (
+      'DSBplot-process -o {} -i {} {} {} {} --ref {} --dsb 67 --reads {} {} {} {}'
+      .format(output, *input_sam, ref, *reads)
+    )
+    print('Testing command: ' + command)
+    self.assertEqual(os.system(command), 0, 'DSBplot-process failed.')
+
+    # Make sure output is the same as expected
     for file in TestProcess.get_check_files():
       print('Testing file: ' + file)
       self.check_equality(os.path.join(output, file), os.path.join(output_expected, file))
